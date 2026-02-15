8b76a033791bec804ef9b09d9e3e3cb5ae57c403
Ayush Goyal <itsayushgoyalskb@gmail.com>
2026-02-16 00:09:28 +0530

    start.sh modified

--- startup/start.sh
+++ startup/start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
+cd ~/workspace
+colcon build --packages-select aruco_mission_cpp graph_traversal
+source install/setup.bash 
+cd ~/ros2_ws/src/icuas26_competition/startup
+source _setup.sh
 # Absolute path to this script. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f $0)
 # Absolute path this script is in. /home/user/bin
-SCRIPTPATH=`dirname $SCRIPT`
+SCRIPTPATH=dirname $SCRIPT
 cd "$SCRIPTPATH"
 
 # remove the old link
@@ -15,5 +19,5 @@ ln session.yml .tmuxinator.yml
 SETUP_NAME=$1
 [ -z "$SETUP_NAME" ] && SETUP_NAME=_setup.sh 
 
-# start tmuxinator
-tmuxinator icuas_competition_example.yml setup_name=$SETUP_NAME 
+# staxrt tmuxinator
+tmuxinator icuas_competition_example.yml setup_name=$SETUP_NAME
