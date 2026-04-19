df0de1ce294c6b1b4f48b5b9134a9a5c49f96031
Shivang <shivangso23@iitk.ac.in>
2026-04-19 22:40:04 +0200

    Made utils a library

--- workspace/src/graph_traversal/src/test_utils.cpp
+++ workspace/src/graph_traversal/src/test_utils.cpp
@@ -1,4 +1,4 @@
-#include "../include/utils.hpp"
+#include "utils.hpp"
 #include <iostream>
 
 int main(){
@@ -6,6 +6,6 @@ int main(){
    std::cout<<"CHARGING_FILE: " << get_charging_file() << std::endl;
    std::cout<<"COMM_RANGE: " << get_comm_range() << std::endl;
    std::cout<<"Charging Area Upper Left: " << charging_area_upper_left()[0] << ", " << charging_area_upper_left()[1] << std::endl;
-   std::cout<<"Charging Area Lower Right: " << charging_area_lower_right()[0] << ", " << charging_area_lower_right()[1] << std::endl;
+   std::cout<<"Charging Area Lower Right: " << charging_area_down_right()[0] << ", " << charging_area_down_right()[1] << std::endl;
 }
 
